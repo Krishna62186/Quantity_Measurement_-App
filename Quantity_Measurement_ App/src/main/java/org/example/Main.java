a381e84f0260c0babb4b0df0ee20f8f4ef3cc67e
Krishna62186 <romitrrs05@gmail.com>
2026-04-24 14:31:02 +0530

    UC9

--- Quantity_Measurement_ App/src/main/java/org/example/Main.java
+++ Quantity_Measurement_ App/src/main/java/org/example/Main.java
@@ -1,26 +1,19 @@
-public enum LengthUnit {
+public enum WeightUnit {
 
-    FEET(1.0),
-    INCHES(1.0 / 12.0),
-    YARDS(3.0),
-    CENTIMETERS(1.0 / 30.48);
+    KILOGRAM(1.0),
+    GRAM(0.001),
+    POUND(0.453592);
 
-    private final double toBaseFactor;
+    private final double toBaseFactor; // base = kilogram
 
-    LengthUnit(double toBaseFactor) {
+    WeightUnit(double toBaseFactor) {
         this.toBaseFactor = toBaseFactor;
     }
 
-    /**
-     * Converts a value in this unit to base unit (feet)
-     */
     public double convertToBaseUnit(double value) {
         return value * toBaseFactor;
     }
 
-    /**
-     * Converts a value from base unit (feet) to this unit
-     */
     public double convertFromBaseUnit(double baseValue) {
         return baseValue / toBaseFactor;
     }
